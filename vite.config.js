1fdad4affdfc8ff4ee8f55519316f785769364d7
Rivaroni <michaelrivaroni@gmail.com>
2024-10-22 13:03:38 -0700

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
-	return {
-		plugins: [
-			sveltekit(),
-			// Only include mkcert in development mode
-			mode === 'development' && mkcert(),
-		].filter(Boolean),  // This removes falsy values (like when mkcert isn't included)
-		server: {
-			https: mode === 'development',  // Only enable HTTPS in development
-			proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
-		}
-	};
+    return {
+        plugins: [
+            sveltekit(),
+            // Only include mkcert in development mode
+            mode === 'development' && mkcert(),
+        ].filter(Boolean),  // This removes falsy values (like when mkcert isn't included)
+        server: {
+            // Only enable HTTPS in development, Railway will handle it in production
+            https: mode === 'development',  
+            host: true,  // Make sure to expose your app to the public
+            proxy: {}, // Avoid "can't use Symbol where you need a string" error
+        }
+    };
 });
