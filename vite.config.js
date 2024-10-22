298c8e33f54a51942e34f97d5cbc42957102c20d
Rivaroni <michaelrivaroni@gmail.com>
2024-10-22 12:07:43 -0700

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import mkcert from "vite-plugin-mkcert"
+import mkcert from "vite-plugin-mkcert";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-
-export default defineConfig({
-	plugins: [sveltekit(),
-		mkcert()
-	],
-	server: {
-		https: true,
-		proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
-	},
-})
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [
+			sveltekit(),
+			// Only include mkcert in development mode
+			mode === 'development' && mkcert(),
+		].filter(Boolean),  // This removes falsy values (like when mkcert isn't included)
+		server: {
+			https: mode === 'development',  // Only enable HTTPS in development
+			proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
+		}
+	};
+});
